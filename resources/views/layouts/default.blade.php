b27d49c39737968c678d328917f5fed01148cb73
awatefbouajaja <bouajaja.awatef@gmail.com>
2018-08-23 11:21:45 +0100

    Detup and static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Laracarte</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+    <!-- font-awesome CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<style>
+		body{
+			font-family: 'open Sans',Helvitica,Aria,Sans-serif;
+		}
+		footer{
+			margin: 4em 0;
+		}
+	</style>
+
+</head>
+<body>
+
+@include('layouts/partials/_nav')
+
+@yield('content')
+
+@include('layouts/partials/_footer')
+<script src="//code.jquery.com/jquery.min.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+Route::get('/', [
+	'as'=>'root_path',
+	'uses'=>'PagesController@home'
+]);
+
+Route::get('/about', [
+	'as'=>'about_path',
+	'uses'=>'PagesController@about'
+]);
